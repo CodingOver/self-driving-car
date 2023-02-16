d8db488786b285bf774e353c2ef336663099836e
CodingOver <paypal1business.over@gmail.com>
2023-02-16 21:02:33 +0100

    Creating The Road

--- main.js
+++ main.js
@@ -3,18 +3,20 @@ const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
 canvas.width = 200
 
+// Building Road
+let road = new Road(canvas.width / 2, canvas.width * 0.9)
 // build new car
-let mycar = new Car(canvas.width / 2, canvas.height / 2, 30, 50)
+let car = new Car(canvas.width / 2, canvas.height / 2, 30, 50)
 
 
 
 function animate() {
-    mycar.update()
-
+    car.update()
 
 
     canvas.height = innerHeight
-    mycar.draw(ctx)
+    road.draw(ctx)
+    car.draw(ctx)
     requestAnimationFrame(animate)
 }
 
