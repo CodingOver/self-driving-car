56c8215d321b50927603d951afb2edb056917852
CodingOver <paypal1business.over@gmail.com>
2023-02-16 21:45:28 +0100

    Following the car on the road

--- main.js
+++ main.js
@@ -13,10 +13,15 @@ let car = new Car(road.getLaneCenter(1), 100, 30, 50)
 function animate() {
     car.update()
 
-
     canvas.height = innerHeight
+
+    ctx.save()
+    ctx.translate(0, -car.y + canvas.height * 0.5)
+
     road.draw(ctx)
     car.draw(ctx)
+
+    ctx.restore()
     requestAnimationFrame(animate)
 }
 
