17e4eeaca51ff5f69fdd1df4818640c97ab0f932
CodingOver <paypal1business.over@gmail.com>
2023-02-18 19:46:22 +0100

    Drawing the rays

--- car.js
+++ car.js
@@ -4,54 +4,61 @@ class Car {
         this.y = y
         this.width = width
         this.height = height
-        this.controls = new Controls()
 
         this.speed = 0
         this.acceleration = .2
         this.maxSpeed = 3
         this.friction = 0.05
         this.angle = 0
+
+        this.sensor = new Sensor(this)
+        this.controls = new Controls()
     }
+
     update() {
         this.#move();
+        this.sensor.update()
     }
+
     #move() {
         if (this.controls.forward) {
-            this.speed += this.acceleration
+            this.speed += this.acceleration;
         }
         if (this.controls.reverse) {
-            this.speed -= this.acceleration
+            this.speed -= this.acceleration;
         }
 
         if (this.speed > this.maxSpeed) {
-            this.speed = this.maxSpeed
+            this.speed = this.maxSpeed;
+        }
+        if (this.speed < -this.maxSpeed / 2) {
+            this.speed = -this.maxSpeed / 2;
         }
 
-        if (this.speed < -this.maxSpeed) {
-            this.speed = -this.maxSpeed / 2
-        }
         if (this.speed > 0) {
-            this.speed -= this.friction
+            this.speed -= this.friction;
         }
         if (this.speed < 0) {
-            this.speed += this.friction
+            this.speed += this.friction;
         }
         if (Math.abs(this.speed) < this.friction) {
-            this.speed = 0
+            this.speed = 0;
         }
 
         if (this.speed != 0) {
-            const flip = this.speed > 0 ? 1 : -1
+            const flip = this.speed > 0 ? 1 : -1;
             if (this.controls.left) {
-                this.angle += 0.03 * flip
+                this.angle += 0.03 * flip;
             }
             if (this.controls.right) {
-                this.angle -= 0.03 * flip
+                this.angle -= 0.03 * flip;
             }
-            this.x -= Math.sin(this.angle) * this.speed;
-            this.y -= Math.cos(this.angle) * this.speed
         }
+
+        this.x -= Math.sin(this.angle) * this.speed;
+        this.y -= Math.cos(this.angle) * this.speed;
     }
+
     draw(ctx) {
         ctx.save()
         ctx.translate(this.x, this.y)
@@ -67,5 +74,7 @@ class Car {
         ctx.fill()
 
         ctx.restore()
+
+        this.sensor.draw(ctx)
     }
 }
